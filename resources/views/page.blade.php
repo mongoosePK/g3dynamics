4ea4f7c22b4405b0fe0d92732e0be539ff1b1d6d
BuildTools <unconfigured@null.spigotmc.org>
2018-10-11 14:17:08 -0500

    fixed verbage

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -109,7 +109,7 @@
                     
                     @if($amount == 0 && $pre === true)
                         <div class="col-lg-9 col-md-9 col-sm-12 col-12 page-container">
-                            <div class='col-lg-12 col-md-12 col-sm-12 col-12 page-title'><h2>Your team has completed the pre-registration process, please come back on October 1st, to pay the remainder of the entry fee.</h2></div>
+                            <div class='col-lg-12 col-md-12 col-sm-12 col-12 page-title'><h2>Your team has completed the pre-registration process, please come back soon, to pay the remainder of the entry fee.</h2></div>
                         </div>
                     @endif
 
