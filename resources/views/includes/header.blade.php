1adfc745c3e9c6002bd026b0c9115e89c32f2d21
mongoosePK <wpulkownik@gmail.com>
2018-10-15 14:14:03 -0500

    rework header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,14 +1,20 @@
-<header class='col-lg-12 col-md-12 col-sm-12 col-12 no-padding inner-wrap'>
-    <div class='logo col-lg-4 col-md-4 col-sm-4 col-6 no-padding float-left'>
-        <a href='/' class='col-lg-12 col-md-12 col-sm-12 col-12 no-padding float-left'>
-            <div class='col-lg-4 col-md-3 col-sm-4 col-3 no-padding float-left'>
-                <img class="logo-image" src="/storage/images/g3-header.png"/>
+<header class="container-fluid no-padding">
+    <div class="container-fluid no-padding">
+        <div class="row align-items-center justify-content-between">
+            <div class="logo col-auto col-md-4 no-padding float-left">
+                <a href="/">
+                    <div class="col-3 no-padding float-left">
+                        <img class="logo-image" src="/storage/images/g3-header.png"/>
+                    </div>
+                    <div class="col-9 d-none d-md-block no-padding float-left logo-text">
+                        <div class="logo-font"><span>G3</span> <i>DYNAMICS</i></div>
+                    </div>
+                </a>
             </div>
-            <div class='col-lg-8 col-md-9 col-sm-8 col-9 no-padding float-left logo-text'>
-                <div class='logo-font'><span>G3</span> <i>DYNAMICS</i></div>
+            <!-- <div class="col-lg-5 col-md-4 col-sm-3 col-0 no-padding float-left intentionally-blank">&nbsp;</div> -->
+            <div class="col-auto no-padding float-left menu">
+                {{ menu('main','bootstrap', ['color'=>true,'icon'=>true]) }}
             </div>
-        </a>
+        </div>
     </div>
-    <div class='col-lg-5 col-md-4 col-sm-3 col-0 no-padding float-left intentionally-blank'>&nbsp;</div>
-    <div class='col-lg-3 col-md-4 col-sm-5 col-6 no-padding float-left menu'> {{ menu('main','bootstrap', ['color'=>true,'icon'=>true]) }}</div>
 </header>
